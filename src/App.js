374b76b2661cba710c8c416d112f67df5cd5cc89
Stan <5821731+stanmbrs@users.noreply.github.com>
2022-01-20 16:18:57 +0100

    Move ToDo component into a file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import './App.css';
+import ToDo from './ToDo';
 
 function AddButton(props) {
   return (
     <input type="button" value={props.value} onClick={props.onClick} />
-  );
-}
-
-function ToDo(props) {
-  console.log(props.onChange);
-  return (
-    <div className="ToDo">
-      <input type="checkbox" name="done" checked={props.done} onChange={props.onChange} />
-      <input type="text" name="todo" value={props.value} onChange={props.onChange} />
-      <input type="button" name="delete" value="🗑" onClick={props.onClick}/>
-    </div>
   );
 }
 
---
+++ src/ToDo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ToDo(props) {
+    console.log(props.onChange);
+    return (
+        <div className="ToDo">
+            <input type="checkbox" name="done" checked={props.done} onChange={props.onChange} />
+            <input type="text" name="todo" value={props.value} onChange={props.onChange} />
+            <input type="button" name="delete" value="🗑" onClick={props.onClick}/>
+        </div>
+    );
+}
+
+export default ToDo;
